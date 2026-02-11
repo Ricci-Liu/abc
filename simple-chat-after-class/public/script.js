156cfcc35f73ed068a3bde139fbeb34d5ed29211
Ricci-Liu <rl4800@nyu.edu>
2026-02-11 12:00:24 +0800

    update port number

--- simple-chat-after-class/public/script.js
+++ simple-chat-after-class/public/script.js
@@ -1,4 +1,14 @@
-let socket = io();
+// lines of code that make sure socket runs on both locan and hongkong server
+// let socket = io();
+// socket connection that works locally and on the server:
+if (
+  location.hostname.toLowerCase().startsWith("browsercircus") ||
+  location.hostname.toLowerCase().startsWith("www")
+) {
+  socket = io({ path: "/ruiqi/port-4260/socket.io" }); // e.g. '/leon/port-4100/socket.io' or '/socket.io'
+} else {
+  socket = io();
+}
 
 let formeElm = document.querySelector("#chatForm");
 console.log(formeElm);
