f1f674dd2f6cdb86ef9a3755b12c27e784952c02
Ricci-Liu <rl4800@nyu.edu>
2026-03-10 14:44:47 +0800

    try again speed

--- project1/public/p1/script.js
+++ project1/public/p1/script.js
@@ -21,7 +21,7 @@ let originX;
 let originY = 0;
 
 let spd = 5;
-let totalPointsNum = 600;
+let totalPointsNum = 550;
 
 let deltaX = 0;
 
@@ -103,7 +103,7 @@ function handleOrientation(eventData) {
 
   testAngle = smoothedAlpha;
 
-  deltaX = map(gyroGamma, -90, 90, -width / 3 - 100, width / 3 + 100);
+  deltaX = map(gyroGamma, -90, 90, -width / 3, width / 3);
   // if (dirRight(gyroAlpha)) {
   //   deltaX = map(gyroBeta, 90, 180, 0, width / 4 - 50);
   // } else {
