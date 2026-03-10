c8a64968e5d453a682fce183af21d8768e298383
Ricci-Liu <rl4800@nyu.edu>
2026-03-10 14:36:24 +0800

    update speed

--- project1/public/p1/script.js
+++ project1/public/p1/script.js
@@ -21,7 +21,7 @@ let originX;
 let originY = 0;
 
 let spd = 5;
-let totalPointsNum = 450;
+let totalPointsNum = 550;
 
 let deltaX = 0;
 
--- project1/public/p3/script.js
+++ project1/public/p3/script.js
@@ -11,7 +11,7 @@ if (
 socket.emit("my-role", { role: "phone3" });
 
 let originX, originY;
-let totalPointsNum = 850;
+let totalPointsNum = 750;
 
 let spd = 5;
 let strokeW = 10;
